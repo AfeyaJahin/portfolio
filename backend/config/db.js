0985bb9e3e17f72efff88e6a6bc427c256276bbd
AfeyaJahin <jahin22a@mtholyoke.edu>
2024-06-12 18:35:50 -0400

    add logs to config db

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,32 @@
+// const mongoose = require('mongoose');
+
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect(process.env.DB_URI);
+//     console.log('MongoDB Connected...');
+//   } catch (err) {
+//     console.error(err.message);
+//     process.exit(1); // Exit process with failure
+//   }
+// };
+
+// module.exports = connectDB;
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.DB_URI);
+    console.time('MongoDB Connection');
+    await mongoose.connect(process.env.DB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    });
+    console.timeEnd('MongoDB Connection');
     console.log('MongoDB Connected...');
   } catch (err) {
-    console.error(err.message);
+    console.error('Error connecting to MongoDB:', err.message);
     process.exit(1); // Exit process with failure
   }
 };
